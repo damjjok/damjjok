dd8482522e0d75cf93d0ff568aab792b7f1c6f8b
김다희 <kdahui@gmail.com>
2024-01-29 15:31:26 +0900

    feat:일정 repository 생성

--- server/src/main/java/com/ssafy/server/repository/TruthRoomRepository.java
+++ server/src/main/java/com/ssafy/server/repository/TruthRoomRepository.java
@@ -1,4 +1,13 @@
 package com.ssafy.server.repository;
 
-public interface TruthRoomRepository {
+import com.ssafy.server.entity.ChallengeEntity;
+import com.ssafy.server.entity.ScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TruthRoomRepository extends JpaRepository<ScheduleEntity, Integer> {
+    //해당 챌린지에 종료되지 않은 일정 조회
+    ScheduleEntity findByChallengeIdAndEndDateFalse(ChallengeEntity ChallengeId);
 }
+
