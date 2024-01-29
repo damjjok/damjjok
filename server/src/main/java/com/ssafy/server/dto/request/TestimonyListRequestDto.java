8d658223d791157da72b53726d245b811eb5074c
최명성 <audtjd1893@gmail.com>
2024-01-29 16:44:47 +0900

    Feat : 증언 수정 API 구현

--- server/src/main/java/com/ssafy/server/dto/request/TestimonyListRequestDto.java
+++ server/src/main/java/com/ssafy/server/dto/request/TestimonyListRequestDto.java
@@ -2,12 +2,15 @@ package com.ssafy.server.dto.request;
 
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestimonyListRequestDto {
 
     @NotBlank
