36c488c9d8dbe908645d7678f4ae776710059fac
김다희 <kdahui@gmail.com>
2024-01-30 12:27:10 +0900

    Fix: 일정 서비스단 오류 수정

--- server/src/main/java/com/ssafy/server/dto/response/ScheduleCreateResponseDto.java
+++ server/src/main/java/com/ssafy/server/dto/response/ScheduleCreateResponseDto.java
@@ -8,10 +8,14 @@ import org.springframework.http.ResponseEntity;
 @Getter
 public class ScheduleCreateResponseDto extends ResponseDto {
     public ScheduleCreateResponseDto() {super();}
-
     public static ResponseEntity<ScheduleCreateResponseDto> success() {
         ScheduleCreateResponseDto responseBody = new ScheduleCreateResponseDto();
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    public static ResponseEntity<ScheduleCreateResponseDto> wrongDate() {
+        ScheduleCreateResponseDto responseBody = new ScheduleCreateResponseDto();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
     }
 
 }
