f5c3c933c2a10cb3f190a8eff87b2c232c0128ed
최명성 <audtjd1893@gmail.com>
2024-01-30 15:05:37 +0900

    Feat : 증거 상세 API 구현

---
+++ server/src/main/java/com/ssafy/server/dto/proof/EvidenceDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.server.dto.proof;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EvidenceDto {
+    private int evidenceId;
+    private String evidenceTitle;
+    private String imagePath;
+    private LocalDateTime imageDate;
+    private int createdBy;
+}
--- server/src/main/java/com/ssafy/server/service/implement/EvidenceServiceImpl.java
+++ server/src/main/java/com/ssafy/server/service/implement/EvidenceServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ssafy.server.service.implement;
 
 import com.ssafy.server.dto.ResponseDto;
+import com.ssafy.server.dto.proof.EvidenceDto;
 import com.ssafy.server.dto.request.proof.EvidenceCreateRequestDto;
+import com.ssafy.server.dto.request.proof.EvidenceDetailRequestDto;
 import com.ssafy.server.dto.request.proof.EvidenceModifyRequestDto;
 import com.ssafy.server.dto.response.proof.EvidenceCreateResponseDto;
+import com.ssafy.server.dto.response.proof.EvidenceDetailResponseDto;
 import com.ssafy.server.dto.response.proof.EvidenceModifyResponseDto;
 import com.ssafy.server.entity.ChallengeEntity;
 import com.ssafy.server.entity.EvidenceEntity;
@@ -115,5 +118,31 @@ public class EvidenceServiceImpl implements EvidenceService {
         }
 
         return EvidenceModifyResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super EvidenceDetailResponseDto> detailEvidence(EvidenceDetailRequestDto dto) {
+        EvidenceDto evidence = null;
+        try{
+            int evidenceId = dto.getEvidenceId();
+
+            EvidenceEntity evidenceEntity = evidenceRepository.findByEvidenceId(evidenceId);
+
+            if(evidenceEntity == null) return ResponseDto.validationFail();
+
+            evidence = new EvidenceDto();
+            evidence.setEvidenceId(evidenceEntity.getEvidenceId());
+            evidence.setEvidenceTitle(evidenceEntity.getEvidenceTitle());
+            evidence.setImagePath(evidenceEntity.getImagePath());
+            evidence.setImageDate(evidenceEntity.getImageDate());
+            evidence.setCreatedBy(evidenceEntity.getCreatedBy());
+
+
+        }catch(Exception exception){
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return EvidenceDetailResponseDto.success(evidence);
     }
 }
