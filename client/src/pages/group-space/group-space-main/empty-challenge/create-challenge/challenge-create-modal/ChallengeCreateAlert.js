82029fa119fc8fa00db9c2e44fa03538031ca459
jongmin Son <kain9101@naver.com>
2024-01-31 00:32:24 +0900

    Init: ChallengeCreateAlert.js 생성

---
+++ client/src/pages/group-space/group-space-main/empty-challenge/create-challenge/challenge-create-modal/ChallengeCreateAlert.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    // AlertDialogCloseButton,
+    useDisclosure,
+} from "@chakra-ui/react";
+
+import NormalButton from "../../../../components/button/normalbutton";
+import ChallengeCreateModal from "./ChallengeCreateModal";
+
+function ChallengeCreateAlert() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = React.useRef();
+
+    return (
+        <>
+            <NormalButton
+                onClick={onOpen}
+                buttonName={"챌린지 생성하기"}
+                variant={"bigbtn"}
+            />
+
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            챌린지 생성하기
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            정말로 챌린지를 생성하시겠습니까? 생성하신 후에는
+                            도전 내용을 변경할 수 없습니다.
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <ChallengeCreateModal />
+                            <NormalButton
+                                onClick={onClose}
+                                buttonName={"취소"}
+                                variant={"cancel"}
+                            />
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
+    );
+}
+
+export default ChallengeCreateAlert;
