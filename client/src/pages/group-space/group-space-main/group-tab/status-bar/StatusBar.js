12c84727408470b592cd79a7ffe943247baaf7ab
jongmin Son <kain9101@naver.com>
2024-01-31 00:45:16 +0900

    Init: grouptab/StatusBar 생성

---
+++ client/src/pages/group-space/group-space-main/group-tab/status-bar/StatusBar.js
@@ -0,0 +1,35 @@
+import { useRecoilValue } from "recoil";
+import { currentUserState } from "../../../context/user";
+import { challengeState } from "../../../context/Challenge";
+import NormalButton from "../../../components/button/normalbutton";
+
+function StatusBar() {
+    const currentUser = useRecoilValue(currentUserState);
+    const currentChallenge = useRecoilValue(challengeState);
+    let today = new Date();
+
+    return (
+        <div className=" flex justify-between items-center bg-damgray px-4">
+            <div className="flex items-center">
+                <p className=" text-lg font-bold">
+                    {currentUser.userName} 챌린지 -
+                    {currentChallenge.createdAt.toLocaleDateString()}
+                </p>
+                <div className=" bg-damblack rounded-xl max-h-4 px-2 mx-2 text-xs text-damyellow">
+                    D-{today.getDate() - currentChallenge.createdAt.getDate()}
+                </div>
+                <p>{currentChallenge.determination}</p>
+                <button>수정</button>
+            </div>
+            <div className="flex items-center">
+                <NormalButton buttonName={"출석하기"} variant={"smbtn"} />
+                <div>
+                    <div>리워드</div>
+                    <p>리워드 숫자</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default StatusBar;
