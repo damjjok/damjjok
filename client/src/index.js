436fec638d74ba63954a782042fb29587392febc
moonwlgh <moonwlgh@gmail.com>
2024-01-30 12:05:06 +0900

    Init: index.js 에 BrowerRouter 추가

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <RecoilRoot>
-            <App />
-        </RecoilRoot>
+        <BrowserRouter>
+            <RecoilRoot>
+                <App />
+            </RecoilRoot>
+        </BrowserRouter>
     </React.StrictMode>,
 );
 
