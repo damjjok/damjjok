ba65b41887ec378567500ed80944df2de109fcbe
김영후 <eagle7449@naver.com>
2024-01-31 16:54:50 +0900

    Feat: 스트릭 컴포넌트 생성 및 출석부 컴포넌트 이름 변경

--- client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
+++ client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
@@ -4,7 +4,7 @@ import { useRecoilValue } from "recoil";
 import { stepState } from "contexts/TruthRoom";
 import JoinMemberComponent from "./inner-components/join-member/JoinMemberComponent";
 import ReportComopnent from "./inner-components/report/ReportComponent";
-import CalanderComponent from "./inner-components/calander/CalenderComponent";
+import AttendanceComponent from "./inner-components/attendance/AttendanceComponent";
 
 function LeftComponent() {
     const step = useRecoilValue(stepState);
@@ -12,7 +12,7 @@ function LeftComponent() {
     return (
         <Wrapper>
             {step === 1 && <ReportComopnent />}
-            {(step === 4 || step === 5) && <CalanderComponent />}
+            {(step === 4 || step === 5) && <AttendanceComponent />}
             {step !== 1 && step !== 4 && step !== 5 && <JoinMemberComponent />}
         </Wrapper>
     );
