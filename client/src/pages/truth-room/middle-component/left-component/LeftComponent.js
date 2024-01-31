31493bf18096bb00ac63ffea3122404eb1dfc8a2
김영후 <eagle7449@naver.com>
2024-01-31 12:25:42 +0900

    Init: LeftComponent에 step 별 조건부 랜더링 적용

--- client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
+++ client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
@@ -3,13 +3,15 @@ import { Wrapper } from "./LeftComponent.style";
 import { useRecoilValue } from "recoil";
 import { stepState } from "contexts/TruthRoom";
 import JoinMemberComponent from "./inner-components/JoinMemberComponent";
+import ReportComopnent from "./inner-components/ReportComponent";
 
 function LeftComponent() {
     const step = useRecoilValue(stepState);
 
     return (
         <Wrapper>
-            {step === 1 && <JoinMemberComponent></JoinMemberComponent>}
+            {step === 1 && <ReportComopnent />}
+            {step !== 1 && step !== 4 && step !== 5 && <JoinMemberComponent />}
         </Wrapper>
     );
 }
