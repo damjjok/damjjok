d867a425acb6a16b48e3daed5ac4fc7b30ff5700
김영후 <eagle7449@naver.com>
2024-01-31 12:45:56 +0900

    Init: Right component 생성 및 TruthRoom에 등록

--- client/src/pages/truth-room/middle-component/MiddleComponent.js
+++ client/src/pages/truth-room/middle-component/MiddleComponent.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Wrapper } from "./MiddleComponent.style";
 import LeftComponent from "./left-component/LeftComponent";
+import RightComponent from "./right-component/RightComponent";
 
 function MiddleComponent() {
     return (
         <Wrapper>
             <LeftComponent></LeftComponent>
+            <RightComponent></RightComponent>
         </Wrapper>
     );
 }
--- client/src/pages/truth-room/middle-component/right-component/RightComponent.js
+++ client/src/pages/truth-room/middle-component/right-component/RightComponent.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Wrapper } from "./RightComponent.style";
+import RtcComponent from "./rtc-component/RtcComponent";
 
 function RightComponent() {
-    return <Wrapper></Wrapper>;
+    return (
+        <Wrapper>
+            <RtcComponent></RtcComponent>
+        </Wrapper>
+    );
 }
 
 export default RightComponent;
