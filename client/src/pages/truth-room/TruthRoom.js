fe9f9286dfb6ffdc90ad992539973c619f8d428d
김영후 <eagle7449@naver.com>
2024-01-31 11:20:11 +0900

    TruthRoom에 TopComponent 추가

--- client/src/pages/truth-room/TruthRoom.js
+++ client/src/pages/truth-room/TruthRoom.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Wrapper } from "./TruthRoom.style";
+import TopComponent from "./top-component/TopComponent";
 
 function TruthRoom() {
-    return <div></div>;
+    return (
+        <Wrapper>
+            <TopComponent></TopComponent>
+        </Wrapper>
+    );
 }
 
 export default TruthRoom;
