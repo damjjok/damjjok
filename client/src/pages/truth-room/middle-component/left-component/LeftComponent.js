c6ea530773baebdbcc352e474edeabb5b7441b87
김영후 <eagle7449@naver.com>
2024-01-31 12:28:18 +0900

    Init: CalanderComponent 생성 및 LeftComponent에 등록

--- client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
+++ client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
@@ -4,6 +4,7 @@ import { useRecoilValue } from "recoil";
 import { stepState } from "contexts/TruthRoom";
 import JoinMemberComponent from "./inner-components/JoinMemberComponent";
 import ReportComopnent from "./inner-components/ReportComponent";
+import CalanderComponent from "./inner-components/CalenderComponent";
 
 function LeftComponent() {
     const step = useRecoilValue(stepState);
@@ -11,6 +12,7 @@ function LeftComponent() {
     return (
         <Wrapper>
             {step === 1 && <ReportComopnent />}
+            {(step === 4 || step === 5) && <CalanderComponent />}
             {step !== 1 && step !== 4 && step !== 5 && <JoinMemberComponent />}
         </Wrapper>
     );
---
+++ client/src/pages/truth-room/middle-component/left-component/inner-components/CalenderComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Wrapper } from "./InnerComponent.style";
+
+function CalanderComponent(props) {
+    return <Wrapper>달력</Wrapper>;
+}
+
+export default CalanderComponent;
