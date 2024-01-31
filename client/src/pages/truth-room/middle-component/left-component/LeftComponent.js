540118e696855e1f4fc808db8cebddedcaeeb00e
김영후 <eagle7449@naver.com>
2024-01-31 11:36:39 +0900

    Init: LeftComponent에 recoil value step 등록

--- client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
+++ client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { Wrapper } from "./LeftComponent.style";
+import { useRecoilValue } from "recoil";
+import { stepState } from "contexts/TruthRoom";
 
 function LeftComponent() {
+    const step = useRecoilValue(stepState);
+
     return <Wrapper></Wrapper>;
 }
 
