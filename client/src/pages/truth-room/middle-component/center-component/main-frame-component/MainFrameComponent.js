23e5a8ad090fbf5649789479f500cefe8810b4f0
김영후 <eagle7449@naver.com>
2024-01-31 14:08:58 +0900

    MainFrameComponent, styled div 생성

---
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/MainFrameComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Wrapper } from "./MainFrameComponent.style";
+
+function MainFrameComponent() {
+    return <Wrapper></Wrapper>;
+}
+
+export default MainFrameComponent;
