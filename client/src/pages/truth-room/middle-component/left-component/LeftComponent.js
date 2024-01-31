301fd16eee6367a1f71866e5ac13af03aac89c8a
김영후 <eagle7449@naver.com>
2024-01-31 11:45:21 +0900

    Init: JoinMemberComponent 생성 및 LeftComponent에 등록

--- client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
+++ client/src/pages/truth-room/middle-component/left-component/LeftComponent.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Wrapper } from "./LeftComponent.style";
 import { useRecoilValue } from "recoil";
 import { stepState } from "contexts/TruthRoom";
+import JoinMemberComponent from "./inner-components/JoinMemberComponent";
 
 function LeftComponent() {
     const step = useRecoilValue(stepState);
 
-    return <Wrapper></Wrapper>;
+    return (
+        <Wrapper>
+            {step === 1 && <JoinMemberComponent></JoinMemberComponent>}
+        </Wrapper>
+    );
 }
 
 export default LeftComponent;
