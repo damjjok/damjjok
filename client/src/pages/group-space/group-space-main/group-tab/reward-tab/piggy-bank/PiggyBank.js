287e5579c63c24abb807b3c1ff6cc5c7c4b86b3d
손종민 <kain9101@naver.com>
2024-02-01 16:16:08 +0900

    feat: Piggybank 이미지 위에 현재 누적 금액 표기 기능

--- client/src/pages/group-space/group-space-main/group-tab/reward-tab/piggy-bank/PiggyBank.js
+++ client/src/pages/group-space/group-space-main/group-tab/reward-tab/piggy-bank/PiggyBank.js
@@ -1,18 +1,46 @@
 // import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
 import piggybanklogo from "assets/images/piggybanklogo.png";
+// import { challengeState } from "contexts/Challenge";
+// import { useRecoilValue } from "recoil";
 // import { css } from "twin.macro";
 
-function PiggyBank(progress = 0.4) {
+function PiggyBank(progress = 0.2) {
     const processPercentage = progress * 100;
+    const challengeList = localStorage.getItem("challengeList");
+    let currentChallenge = {};
+    let today = new Date();
+
+    if (challengeList) {
+        // 가져온 값이 있으면 JSON.parse를 사용해서 문자열을 객체로 변환합니다.
+        const myChallenge = JSON.parse(challengeList);
+
+        // 이후 myObject를 원하는대로 사용할 수 있습니다.
+        currentChallenge = myChallenge[0];
+    } else {
+        console.log("No data in localStorage");
+    }
+
+    const startedDate = new Date(currentChallenge.createdAt);
+    // 두 날짜 사이의 밀리초 차이를 계산합니다.
+    const diffMilliseconds = today.getTime() - startedDate.getTime();
+    const diffDays = Math.floor(diffMilliseconds / (24 * 60 * 60 * 1000));
 
     return (
-        <div className="flex justify-center my-8">
+        <div className="relative flex justify-center my-8">
             <div style={{ width: "300px" }}>
                 <img
                     src={piggybanklogo}
                     alt="Piggybank Background"
                     style={{ width: `${processPercentage}%` }}
                 />
+            </div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] h-fit text-center py-4">
+                <p className=" font-bold">
+                    누적 금액 :
+                    {currentChallenge.initialMoney +
+                        currentChallenge.savedMoney *
+                            (diffDays / currentChallenge.savedPeriod)}
+                </p>
             </div>
         </div>
     );
