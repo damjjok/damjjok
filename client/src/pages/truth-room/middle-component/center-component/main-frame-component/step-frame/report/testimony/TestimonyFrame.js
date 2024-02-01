5adb1451eae3ed9738e00c728c9d809575f86607
김영후 <eagle7449@naver.com>
2024-02-01 12:39:14 +0900

    Feat: 증언 컴포넌트 버튼 배치

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/TestimonyFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/TestimonyFrame.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import { Wrapper } from "./TestimonyFrame.style";
 import { useRecoilValue } from "recoil";
 import { testimonyState } from "contexts/TruthRoom";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 function TestimonyFrame(props) {
     // 이전, 다음 버튼과 중앙에는 증거 상세 내용 보는 컴포넌트
     const testimonies = useRecoilValue(testimonyState);
     const [testimonyIdx, setTestimonyIdx] = useState(0); // 보여 줄 증거의 인덱스
 
-    return <Wrapper></Wrapper>;
+    return (
+        <Wrapper>
+            <ChevronLeftIcon boxSize={"100px"} />
+            <ChevronRightIcon boxSize={"100px"} />
+        </Wrapper>
+    );
 }
 
 export default TestimonyFrame;
---
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/detail/TestimonyDeatilComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TestimonyDeatilComponent(props) {
+    return <div></div>;
+}
+
+export default TestimonyDeatilComponent;
