3b40c74ac4b86efeb54762d891e17edff84743c0
김영후 <eagle7449@naver.com>
2024-02-01 11:34:40 +0900

    Feat:좌측 컴포넌트에서 목격 사진, 목격담 탭 누를 때 증거, 증언 모드 전역 상태 변경 함수 등록

--- client/src/pages/truth-room/middle-component/left-component/inner-components/report/tab/ReportTab.js
+++ client/src/pages/truth-room/middle-component/left-component/inner-components/report/tab/ReportTab.js
@@ -2,11 +2,26 @@ import React from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import TestimonyComponent from "../testimony/TestimonyComponent";
 import EvidenceComponent from "../evidence/EvidenceComponent";
+import { useSetRecoilState } from "recoil";
+import { reportModeState } from "contexts/TruthRoom";
 
 function ReportTab(props) {
+    const setReportMode = useSetRecoilState(reportModeState);
+
+    function changeReportMode(mode) {
+        setReportMode(mode);
+        console.log("report mode changed to " + mode);
+    }
+
     return (
         <div>
-            <Tabs isFitted variant="enclosed">
+            <Tabs
+                isFitted
+                variant="enclosed"
+                onChange={(index) =>
+                    changeReportMode(index === 0 ? "EVIDENCE" : "TESTIMONY")
+                }
+            >
                 <TabList mb="1em">
                     <Tab>목격 사진</Tab>
                     <Tab>목격담</Tab>
