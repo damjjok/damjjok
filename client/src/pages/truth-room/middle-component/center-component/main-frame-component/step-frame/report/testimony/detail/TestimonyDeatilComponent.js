1c129f77b60fb39f9290e418904afa8069895560
김영후 <eagle7449@naver.com>
2024-02-01 14:38:10 +0900

    Fix: TestimonyDetail style component화

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/detail/TestimonyDeatilComponent.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/detail/TestimonyDeatilComponent.js
@@ -4,14 +4,8 @@ import { Wrapper } from "./TestimonyDetail.style";
 function TestimonyDeatilComponent({ testimony }) {
     return (
         <Wrapper>
-            <div style={{ margin: "20px" }}>제목: {testimony.title}</div>
-            <div
-                style={{
-                    margin: "20px",
-                    height: "400px",
-                    border: "1px solid black",
-                }}
-            >
+            <div className="title-component">제목: {testimony.title}</div>
+            <div className="content-component">
                 <div>내용</div>
                 {testimony.content}
             </div>
--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/detail/TestimonyDetail.style.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/detail/TestimonyDetail.style.js
@@ -4,4 +4,14 @@ export const Wrapper = styled.div`
     width: 460px;
     height: 552px;
     border: 1px solid black;
+
+    .title-component {
+        margin: 20px;
+    }
+
+    .content-component {
+        margin: 20px;
+        height: 400px;
+        border: 1px solid black;
+    }
 `;
