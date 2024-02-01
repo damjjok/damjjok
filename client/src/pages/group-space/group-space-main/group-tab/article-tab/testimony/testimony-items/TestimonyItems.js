d89bddbace3d32660e42610ee1379f8fb6b7d5a6
문지호 <moonwlgh@gmail.com>
2024-02-01 22:17:31 +0900

    Feat: TestimonyDetailModal.js 추가

--- client/src/pages/group-space/group-space-main/group-tab/article-tab/testimony/testimony-items/TestimonyItems.js
+++ client/src/pages/group-space/group-space-main/group-tab/article-tab/testimony/testimony-items/TestimonyItems.js
@@ -1,9 +1,18 @@
-import { Card, CardBody, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+    Card,
+    CardBody,
+    Stack,
+    Heading,
+    Text,
+    useDisclosure,
+} from "@chakra-ui/react";
+import TestimonyDetailModal from "../testimony-modal/TestimonyDetailModal";
 
 const TestimonyItems = ({ title, content }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
         <div className="TestimonyItems">
-            <Card maxW="sm">
+            <Card maxW="sm" onClick={onOpen}>
                 <CardBody>
                     <Stack mt="6" spacing="3">
                         <Heading size="md">{title}</Heading>
@@ -11,6 +20,13 @@ const TestimonyItems = ({ title, content }) => {
                     </Stack>
                 </CardBody>
             </Card>
+
+            <TestimonyDetailModal
+                isOpen={isOpen}
+                onClose={onClose}
+                title={title}
+                content={content}
+            />
         </div>
     );
 };
