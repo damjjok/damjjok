1d7cd0f66f3ac50c90e6234a48012c1c14fcd904
김영후 <eagle7449@naver.com>
2024-02-01 11:49:19 +0900

    Feat: TestimonyFrame에 전역 testimony 변수 호출, 보여줄 증거 인덱스용 변수 선언

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/TestimonyFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/testimony/TestimonyFrame.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Wrapper } from "./TestimonyFrame.style";
+import { useRecoilValue } from "recoil";
+import { testimonyState } from "contexts/TruthRoom";
 
 function TestimonyFrame(props) {
+    // 이전, 다음 버튼과 중앙에는 증거 상세 내용 보는 컴포넌트
+    const testimonies = useRecoilValue(testimonyState);
+    const [testimonyIdx, setTestimonyIdx] = useState(0); // 보여 줄 증거의 인덱스
+
     return <Wrapper></Wrapper>;
 }
 
