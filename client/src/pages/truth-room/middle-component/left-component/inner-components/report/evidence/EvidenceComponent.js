bb5cff54392140a9b4ef3ae52b3681631971438e
김영후 <eagle7449@naver.com>
2024-02-01 17:44:14 +0900

    Feat: 좌측 컴포넌트에서 사진 선택 시 중앙에 렌더링 되는 기능 추가

--- client/src/pages/truth-room/middle-component/left-component/inner-components/report/evidence/EvidenceComponent.js
+++ client/src/pages/truth-room/middle-component/left-component/inner-components/report/evidence/EvidenceComponent.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { Wrapper } from "../TabComponent.style";
-import { useRecoilValue } from "recoil";
-import { evidenceState } from "contexts/TruthRoom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { evidenceIndexState, evidenceState } from "contexts/TruthRoom";
 import EvidenceFrame from "./EvidenceFrame";
 
 function EvidenceComponent(props) {
     const evidences = useRecoilValue(evidenceState);
+    const setEvidenceIdx = useSetRecoilState(evidenceIndexState);
+
+    function handlerEvidenceClick(index) {
+        setEvidenceIdx(index);
+        console.log("인덱스 변경: " + index);
+    }
 
     return (
         <Wrapper>
-            {evidences.map((evidence) => (
-                <EvidenceFrame evidence={evidence}></EvidenceFrame>
+            {evidences.map((evidence, index) => (
+                <EvidenceFrame
+                    key={index}
+                    evidence={evidence}
+                    onClick={() => handlerEvidenceClick(index)}
+                ></EvidenceFrame>
             ))}
         </Wrapper>
     );
--- client/src/pages/truth-room/middle-component/left-component/inner-components/report/evidence/EvidenceFrame.js
+++ client/src/pages/truth-room/middle-component/left-component/inner-components/report/evidence/EvidenceFrame.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Wrapper } from "../Frame.style";
 
-function EvidenceFrame({ evidence }) {
+function EvidenceFrame({ evidence, onClick }) {
     return (
-        <Wrapper>
+        <Wrapper onClick={onClick}>
             <div>{evidence.title}</div>
             <div>
                 <img src={evidence.img} alt="에러"></img>
