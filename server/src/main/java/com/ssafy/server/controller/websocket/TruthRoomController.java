e185d726e7994bd0ec20ee0eaa3353b39f1b8d29
김다희 <kdahui@gmail.com>
2024-02-02 21:34:50 +0900

    Feat:입장 후 준비단계

--- server/src/main/java/com/ssafy/server/controller/websocket/TruthRoomController.java
+++ server/src/main/java/com/ssafy/server/controller/websocket/TruthRoomController.java
@@ -5,6 +5,7 @@ import com.ssafy.server.service.EnterRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -20,10 +21,24 @@ public class TruthRoomController {
     private final EnterRoomService enterRoomService;
     private final SimpMessageSendingOperations messagingTemplate;
 
-    @MessageMapping("/enter/{roomId}")
-    public void enter(@DestinationVariable Integer roomId,String userName, TruthRoomDto dto, SimpMessageHeaderAccessor headerAccessor) {
+    @MessageMapping("/enter/{userName}")
+    public void enter(@DestinationVariable String userName, TruthRoomDto dto, SimpMessageHeaderAccessor headerAccessor) {
         String sessionId = headerAccessor.getSessionId();
+        Integer roomId = dto.getRoomId();
         enterRoomService.createOrGetRoom(roomId);
         enterRoomService.addMember(roomId, sessionId, userName);
+        //입장 목록 보내주기
+        messagingTemplate.convertAndSend("/topic/member",enterRoomService.createOrGetRoom(roomId));
+    }
+
+    @MessageMapping("/ready")
+    public void ready(TruthRoomDto dto, SimpMessageHeaderAccessor headerAccessor){
+        String sessionId = headerAccessor.getSessionId();
+        Integer roomId = dto.getRoomId();
+        enterRoomService.setMemberReady(roomId, sessionId, true);
+        if(enterRoomService.areAllMemberReadey(roomId)) {
+            //준비했을때 모든 인원이 준비라면 모두에게 true 보내주기
+            messagingTemplate.convertAndSend("/app/readyState", true);
+        }
     }
 }
