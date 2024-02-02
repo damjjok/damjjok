cb831f82c1787afc77e13d10531fbcb8aa76fe98
moonwlgh <moonwlgh@gmail.com>
2024-02-02 11:44:36 +0900

    Fix: TestimonyCreateModal.js css 수정

--- client/src/pages/group-space/group-space-main/group-tab/article-tab/ArticleTab.js
+++ client/src/pages/group-space/group-space-main/group-tab/article-tab/ArticleTab.js
@@ -1,16 +1,16 @@
 import Evidence from "./evidence/Evidence";
 import Testimony from "./testimony/Testimony";
+import { VStack } from "@chakra-ui/react";
 
 function ArticleTab() {
     return (
         <div className="ArticleTab">
             <div>증언 증거 제보 컴포넌트입니당</div>
-            <div className="Tesimony">
+            <VStack spacing={20} align="stretch">
                 <Testimony />
-            </div>
-            <div className="Evidence">
+
                 <Evidence />
-            </div>
+            </VStack>
         </div>
     );
 }
