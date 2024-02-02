0e5f8de3a4f1e6dd4d6270180264e3abea1a825d
김영후 <eagle7449@naver.com>
2024-02-02 14:03:02 +0900

    Fix: 투표 총 명수 group 기반 변수처리

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/vote/VoteWaitComponent.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/vote/VoteWaitComponent.js
@@ -1,6 +1,16 @@
+import { groupState, stepState } from "contexts/TruthRoom";
 import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 function VoteWaitComponent(props) {
+    const [step, setStep] = useRecoilState(stepState);
+    const group = useRecoilValue(groupState);
+
+    function handleTestClick() {
+        // 다음 단계로 넘어가기 위한 테스트 함수
+        setStep(step + 1);
+    }
+
     return (
         <div
             style={{
@@ -12,7 +22,12 @@ function VoteWaitComponent(props) {
             <div style={{ margin: "10px", textAlign: "center" }}>
                 다른 사람들의 투표를 기다리고 있어요...
             </div>
-            <div style={{ margin: "10px", textAlign: "center" }}> 2 / 4</div>
+            <div
+                style={{ margin: "10px", textAlign: "center" }}
+                onClick={handleTestClick}
+            >
+                2 / {group.length - 1}
+            </div>
         </div>
     );
 }
