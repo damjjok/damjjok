d88a90946d7a34d42ae0ec88c19961cecbb9d9a9
손종민 <kain9101@naver.com>
2024-02-02 11:37:49 +0900

    Fix: div HStack과 Box로 변경 및 css 적용

--- client/src/pages/group-space/group-space-main/group-tab/home-tab/HomeTab.js
+++ client/src/pages/group-space/group-space-main/group-tab/home-tab/HomeTab.js
@@ -2,6 +2,9 @@ import { useRecoilValue } from "recoil";
 import Strick from "./strick/Strick";
 import { challengeState } from "../../../../../contexts/Challenge";
 import { currentUserState } from "../../../../../contexts/User";
+import getMoneyGif from "assets/gifs/getMoney.gif";
+import { Box, HStack } from "@chakra-ui/react";
+
 function HomeTab() {
     const currentUser = useRecoilValue(currentUserState);
     let currentChallenge = localStorage.getItem("challengeList");
@@ -39,26 +42,37 @@ function HomeTab() {
                 <p className="text-xl font-bold">
                     오늘의 {currentUser.userName}님은...
                 </p>
-                <div className="flex flex-wrap justify-center">
-                    <div className=" border-4 rounded-xl border-damyellow min-w-[200px] w-[200px] h-[200px] py-20 mx-4 my-4">
-                        <p>이미지</p>
-                        <p className="text-sm font-semibold">
-                            하루 1값 기준 4500원을 아꼈어요!
+                <HStack>
+                    <Box
+                        maxW="xs"
+                        maxH="xs"
+                        className="flex flex-col items-center rounded-3xl border-4 p-8 border-damyellow shadow-xl"
+                    >
+                        <img
+                            src={getMoneyGif}
+                            alt="getMoneyGif"
+                            className="w-[150px] min-w-[150px] p-2"
+                        />
+                        <p className="text-xs text-center font-semibold">
+                            하루 1갑 기준
                         </p>
-                    </div>
-                    <div className=" border-4 rounded-xl border-damyellow min-w-[200px] w-[200px] h-[200px] py-20 mx-4 my-4">
+                        <p className=" text-center font-semibold">
+                            4500원을 아꼈어요!
+                        </p>
+                    </Box>
+                    <Box>
                         <p>이미지</p>
                         <p className="text-sm font-semibold">
                             전체 챌린저 중 상위 n%에요!
                         </p>
-                    </div>
-                    <div className=" border-4 rounded-xl border-damyellow min-w-[200px] w-[200px] h-[200px] py-20 mx-4 my-4">
+                    </Box>
+                    <Box>
                         <p>이미지</p>
                         <p className="text-sm font-semibold">
                             금연 전보다 ㅇㅇ이 n% 좋아졌어요!
                         </p>
-                    </div>
-                </div>
+                    </Box>
+                </HStack>
             </div>
         </div>
     );
