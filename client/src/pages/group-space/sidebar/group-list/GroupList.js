70761bcbed65bbab9698c546e86216c8dfdaedcf
jongmin Son <kain9101@naver.com>
2024-02-04 01:19:35 +0900

    Fix: GroupList 컴포넌트 데이터에 따라 동작하도록 수정

--- client/src/pages/group-space/sidebar/group-list/GroupList.js
+++ client/src/pages/group-space/sidebar/group-list/GroupList.js
@@ -2,9 +2,16 @@ import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { useRecoilValue } from "recoil";
 import { currentGroupState } from "../../../../contexts/User";
+import { useState } from "react";
+
+//테스트를 위한 더미 데이터
+const groupItems = ['E105', 'E106', 'E107']
+
 
 function GroupList() {
     const currentGroup = useRecoilValue(currentGroupState);
+    // 더미데이터이므로 더미데이터[0]으로 초기값 설정, 이후 props나 recoilstate 기반으로 선택한 그룹명이 표시될 수 있도록 수정해야 함
+    const [selectedGroup, setSelectedGroup] = useState(groupItems[0]);
     return (
         <Menu>
             <MenuButton
@@ -12,15 +19,17 @@ function GroupList() {
                 colorScheme="yellow"
                 rightIcon={<ChevronDownIcon />}
             >
-                {currentGroup.groupName}
+                {selectedGroup || currentGroup.groupName}
             </MenuButton>
             <MenuList>
-                <MenuItem minH="48px">
-                    <span>그룹1</span>
+            {
+            groupItems.map((item, index) => (
+                <MenuItem minH="48px" key={index} onClick={() => setSelectedGroup(item)}>
+                    <span>{item}</span>
+                    {/* 해당 그룹으로 가도록 링크도 달아줘야 함 */}
                 </MenuItem>
-                <MenuItem minH="40px">
-                    <span>그룹2</span>
-                </MenuItem>
+            ))
+        }
             </MenuList>
         </Menu>
     );
