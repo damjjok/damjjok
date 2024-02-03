582f2d5f3690d879448dfd9091ea776044359c39
jongmin Son <kain9101@naver.com>
2024-02-04 01:49:36 +0900

    Fix: GroupList Select 사용해서 UI  수정

--- client/src/pages/group-space/sidebar/group-list/GroupList.js
+++ client/src/pages/group-space/sidebar/group-list/GroupList.js
@@ -1,4 +1,4 @@
-import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuList, MenuItem, Button, Select } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { useRecoilValue } from "recoil";
 import { currentGroupState } from "../../../../contexts/User";
@@ -13,25 +13,43 @@ function GroupList() {
     // 더미데이터이므로 더미데이터[0]으로 초기값 설정, 이후 props나 recoilstate 기반으로 선택한 그룹명이 표시될 수 있도록 수정해야 함
     const [selectedGroup, setSelectedGroup] = useState(groupItems[0]);
     return (
-        <Menu>
-            <MenuButton
-                as={Button}
-                colorScheme="yellow"
-                rightIcon={<ChevronDownIcon />}
-            >
-                {selectedGroup || currentGroup.groupName}
-            </MenuButton>
-            <MenuList>
-            {
-            groupItems.map((item, index) => (
-                <MenuItem minH="48px" key={index} onClick={() => setSelectedGroup(item)}>
-                    <span>{item}</span>
-                    {/* 해당 그룹으로 가도록 링크도 달아줘야 함 */}
-                </MenuItem>
+        // <Menu>
+        //     <MenuButton
+        //         as={Button}
+        //         colorScheme="yellow"
+        //         rightIcon={<ChevronDownIcon />}
+        //     >
+        //         {selectedGroup || currentGroup.groupName}
+        //     </MenuButton>
+        //     <MenuList>
+        //     {
+        //     groupItems.map((item, index) => (
+        //         <MenuItem minH="48px" key={index} onClick={() => setSelectedGroup(item)}>
+        //             <span>{item}</span>
+                    /* 해당 그룹으로 가도록 링크도 달아줘야 함 */
+                /* </MenuItem>
             ))
         }
             </MenuList>
-        </Menu>
+        </Menu> */
+
+
+
+        <Select placeholder={selectedGroup || currentGroup.groupName} size='md' bg='dam.yellow'>
+             {
+             groupItems.map((item, index) => (
+                    selectedGroup !== item &&
+                    <option minH="48px" key={index} onClick={() => setSelectedGroup(item)} className=" hover:cursor-pointer">
+                        <span>{item}</span>
+                       {/* 해당 그룹으로 가도록 링크도 달아줘야 함 */}
+                   </option>
+
+                
+            ))
+        }
+        </Select>
+
+
     );
 }
 
