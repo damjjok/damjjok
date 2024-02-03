f991d857a8b0dc86a55bb9820d13c24de35a972d
최명성 <audtjd1893@gmail.com>
2024-02-04 00:41:53 +0900

    Feat : 알람 버튼 추가

--- client/src/pages/group-space/topbar/Topbar.js
+++ client/src/pages/group-space/topbar/Topbar.js
@@ -2,17 +2,44 @@ import { useRecoilValue } from "recoil";
 import BasicButton from "../../../components/button/BasicButton";
 import { currentUserState } from "../../../contexts/User";
 import logo from "assets/images/logo.png";
+import { Button, Flex, Icon, IconButton, Wrap, WrapItem, theme } from "@chakra-ui/react";
+import { BellIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
 
 function Topbar() {
     const currentUser = useRecoilValue(currentUserState);
     return (
         <div className="flex justify-between py-4 border-b border-damlightgray/25">
-            <img src={logo} className="max-h-6 px-4" alt="logo" />
-            <div className="flex px-4">
-                <p className=" text-xs font-semibold px-2">
-                    안녕하세요! {currentUser.userName} 님!
-                </p>
-                <BasicButton buttonName={"로그아웃"} variant={"smbtn"} />
+            <Flex justifyContent={"center"} alignItems={"center"}>
+                <Link to={""}>
+                    <img src={logo} className="max-h-12 px-4" alt="logo" />
+                </Link>
+            </Flex>
+
+            <div className="flex justify-center items-center">
+                <Flex justifyContent={"center"} alignItems={"center"} backgroundColor={"#D9D9D9"} padding={".5em"} borderRadius={"30px"}>
+                    <p className="font-semibold px-2">안녕하세요! {currentUser.userName} 님!</p>
+                    <Button rounded={"20px"} backgroundColor={"#FFD100"} _hover={{ backgroundColor: "#3182CE" }}>
+                        로그아웃
+                    </Button>
+                </Flex>
+                <Wrap>
+                    <Button marginX={"1em"} backgroundColor={"#FFD100"} borderRadius={"50%"} width={"50%"} _hover={{ backgroundColor: "#3182CE" }}>
+                        <BellIcon></BellIcon>
+                        <Wrap
+                            position={"absolute"}
+                            right={"-10%"}
+                            top={"-10%"}
+                            backgroundColor={"red"}
+                            width={"50%"}
+                            height={"50%"}
+                            borderRadius={"50%"}
+                            justify={"center"}
+                        >
+                            <WrapItem>1</WrapItem>
+                        </Wrap>
+                    </Button>
+                </Wrap>
             </div>
         </div>
     );
