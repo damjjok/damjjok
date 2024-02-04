6c76f1584daad195eb46441f0469466452c35cdc
최명성 <audtjd1893@gmail.com>
2024-02-04 22:32:35 +0900

    Fix : Empty Challenge Page 수정

--- client/src/pages/group-space/group-space-main/empty-challenge/EmptyChallenge.js
+++ client/src/pages/group-space/group-space-main/empty-challenge/EmptyChallenge.js
@@ -1,17 +1,26 @@
+import { AddIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { Link, useLocation } from "react-router-dom";
 
 function EmptyChallenge() {
     const location = useLocation();
 
     return (
-        <div>
-            활성화된 챌린지가 없습니다.
-            <Link to={`../create-challenge`}>
-                <div className=" max-w-32 py-8 bg-yellow-400">
-                    <p>새 챌린지 만들기</p>
-                </div>
-            </Link>
-        </div>
+        <Box>
+            <Flex justifyContent={"center"} alignItems={"center"} flexDirection={"column"} height={"90vh"}>
+                <Text fontSize={"30px"} marginBottom={"1rem"}>
+                    활성화된 챌린지가 없습니다.
+                </Text>
+                <Button height={"30%"} backgroundColor={"dam.yellow"} _hover={{ backgroundColor: "#3182CE" }}>
+                    <Link to={`../create-challenge`}>
+                        <Text fontSize={"30px"} marginBottom={"1rem"}>
+                            새 챌린지 만들기
+                        </Text>
+                        <AddIcon fontSize={"5rem"}></AddIcon>
+                    </Link>
+                </Button>
+            </Flex>
+        </Box>
     );
 }
 
