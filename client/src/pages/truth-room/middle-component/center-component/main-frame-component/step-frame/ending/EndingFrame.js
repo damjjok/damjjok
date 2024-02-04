f9c64a20777474f3bf3624e36602785553acc87c
김영후 <eagle7449@naver.com>
2024-02-04 17:26:23 +0900

    Feat: 엔딩 프레임 담쪽이 파트 틀 생성

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/ending/EndingFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/ending/EndingFrame.js
@@ -1,7 +1,23 @@
 import React from "react";
+import EndingDamJJokFrame from "./damJJok/EndingDamJJokFrame";
+import SmallFrameComponent from "../../small-frame/SmallFrameComponent";
 
 function EndingFrame(props) {
-    return <div>엔딩</div>;
+    const testUser = {
+        name: "김영후",
+        role: "damJJok",
+    };
+
+    if (testUser.role === "damJJok") {
+        return (
+            <SmallFrameComponent
+                width={900}
+                height={600}
+                content={<EndingDamJJokFrame />}
+            ></SmallFrameComponent>
+        );
+    } else {
+    }
 }
 
 export default EndingFrame;
---
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/ending/damJJok/EndingDamJJokFrame.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function EndingDamJJokFrame(props) {
+    // 0: 응원해요!
+    // 1: 잔여 적립금은 얼마예요
+    // 2: 새로 도전할래요?
+    const [endingDamJJokStep, setEndingDamJJokStep] = useState(0);
+
+    function handleClickConfirm() {
+        setEndingDamJJokStep(endingDamJJokStep + 1);
+    }
+
+    return (
+        <div>
+            {endingDamJJokStep === 0 && <div>test1</div>}
+            {endingDamJJokStep === 1 && <div>test2</div>}
+            {endingDamJJokStep === 2 && <div>test3</div>}
+        </div>
+    );
+}
+
+export default EndingDamJJokFrame;
