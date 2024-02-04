65d612c14f15aa6eba373f3c4b0822274f83b2f2
jongmin Son <kain9101@naver.com>
2024-02-04 14:30:15 +0900

    Feat: SettingButton Menu UI 적용

--- client/src/pages/group-space/sidebar/setting-button/SettingButton.js
+++ client/src/pages/group-space/sidebar/setting-button/SettingButton.js
@@ -1,5 +1,24 @@
+import { Menu, MenuButton, MenuDivider, MenuItem, MenuList } from "@chakra-ui/react";
+
 function SettingButton() {
-  return <div></div>
+  return <Menu>
+  <MenuButton
+    px={4}
+    py={2}
+    transition='all 0.2s'
+    borderRadius='md'
+    borderWidth='1px'
+    _hover={{ bg: 'gray.400' }}
+    _expanded={{ bg: 'blue.400' }}
+    _focus={{ boxShadow: 'outline' }}
+  >
+  </MenuButton>
+  <MenuList>
+    <MenuItem>그룹원 보기</MenuItem>
+    <MenuDivider />
+    <MenuItem>그룹 초대하기</MenuItem>
+  </MenuList>
+</Menu>
 }
 
 export default SettingButton;
