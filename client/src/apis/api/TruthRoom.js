a5e4c6945d0663e4bc7c5146b4fa9e2f4e4e2fd3
김영후 <eagle7449@naver.com>
2024-02-04 22:40:06 +0900

    Feat: axios 통신 예제 작성

--- client/src/apis/api/TruthRoom.js
+++ client/src/apis/api/TruthRoom.js
@@ -1,1 +1,18 @@
-// 폴더 구조를 나타내기 위해 예시로 생성했습니다.
+import { axiosInstance } from "apis/instance/AxiosInstance";
+
+const postTest = async () => {
+    try {
+        const response = await axiosInstance.post("/test");
+        console.log(response.status);
+    } catch (error) {}
+};
+
+const getTest = async () => {
+    // 이런 식으로 쓰면 된다의 예시
+    try {
+        const response = await axiosInstance.get("/test"); // 호출해온 axiosInstance에 baseURL이 적혀있으므로 그 뒤부터 작성해주면 됨.
+        console.log(response.status);
+    } catch (error) {}
+};
+
+export { postTest, getTest };
--- client/src/apis/instance/AxiosInstance.js
+++ client/src/apis/instance/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const AxiosInstance = axios.create({
-    baseURL: "https://i10e105.p.ssafy.io:8989/api/",
+export const axiosInstance = axios.create({
+    baseURL: "https://i10e105.p.ssafy.io:8989/api",
     timeout: 5000, // 일단 타임아웃 5초로
     // headers: {'our-server-token': 'not-yet'} // 우리 서버 아직 토큰 헤더 이름 안나옴...
 });
