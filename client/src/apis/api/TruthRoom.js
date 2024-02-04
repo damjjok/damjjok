7ce65e498de09bbb6d8a8124a29dbcdd3d45246e
김영후 <eagle7449@naver.com>
2024-02-04 23:10:27 +0900

    Feat: 진실의 방 테스트를 위해 라우터 적용

--- client/src/apis/api/TruthRoom.js
+++ client/src/apis/api/TruthRoom.js
@@ -15,4 +15,23 @@ const getTest = async () => {
     } catch (error) {}
 };
 
-export { postTest, getTest };
+const getSessionId = async (propsSessionKey) => {
+    const body = {
+        sessionKey: propsSessionKey,
+    };
+
+    try {
+        const response = await axiosInstance.post(
+            "/open-vidu/api/sessions",
+            body
+        );
+        console.log(response.status);
+        console.log(response.data);
+        if (response.status === 200) return response.data.sessionKey;
+        else console.log("통신 실패, 상태 코드: " + response.status);
+    } catch (error) {
+        console.log("오픈비두 세션 id 통신에서 에러: " + error);
+    }
+};
+
+export { getSessionId };
--- client/src/pages/truth-room/TruthRoom.js
+++ client/src/pages/truth-room/TruthRoom.js
@@ -3,14 +3,27 @@ import { Wrapper } from "./TruthRoom.style";
 import TopComponent from "./top-component/TopComponent";
 import MiddleComponent from "./middle-component/MiddleComponent";
 import BottomComponent from "./bottom-component/BottomComponent";
+import { Route, Routes } from "react-router-dom";
+import ConnectionTest from "./openvidu/ConnectionTest";
 
 function TruthRoom() {
     return (
-        <Wrapper>
-            <TopComponent></TopComponent>
-            <MiddleComponent></MiddleComponent>
-            <BottomComponent></BottomComponent>
-        </Wrapper>
+        <Routes>
+            <Route
+                path=""
+                element={
+                    <Wrapper>
+                        <TopComponent></TopComponent>
+                        <MiddleComponent></MiddleComponent>
+                        <BottomComponent></BottomComponent>
+                    </Wrapper>
+                }
+            ></Route>
+            <Route
+                path="test"
+                element={<ConnectionTest></ConnectionTest>}
+            ></Route>
+        </Routes>
     );
 }
 
