b7459f5bc0671766a47ff0ac6b347e252ea72d54
김영후 <eagle7449@naver.com>
2024-02-05 11:21:32 +0900

    Feat: 세션 닫기 버튼 생성 및 api 연결

--- client/src/apis/api/TruthRoom.js
+++ client/src/apis/api/TruthRoom.js
@@ -29,4 +29,29 @@ const getSessionId = async (propsSessionKey) => {
     }
 };
 
-export { getSessionId };
+const getOpenviduToken = async (propsSessionId) => {
+    try {
+        const response = await axiosInstance.post(
+            `/v1/sessions/${propsSessionId}/connections`,
+        );
+        if (response.status === 200) return response.data.token;
+        else console.log("통신 실패, 상태 코드: " + response.status);
+    } catch (error) {
+        console.log("오픈비두 토큰 발급 통신에서 에러: " + error);
+    }
+};
+
+const closeOpenviduSession = async (propsSessionKey) => {
+    // 마지막 유저가 나갈 때 실행되는 통신
+    try {
+        const response = await axiosInstance.delete(
+            `/v1/sessions/${propsSessionKey}`,
+        );
+        if (response.status === 200) console.log("세션 닫기 성공");
+        else console.log("통신 실패, 상태 코드: " + response.status);
+    } catch (error) {
+        console.log("오픈비두 세션 닫기 통신에서 에러: " + error);
+    }
+};
+
+export { getSessionId, getOpenviduToken, closeOpenviduSession };
--- client/src/pages/truth-room/openvidu/ConnectionTest.js
+++ client/src/pages/truth-room/openvidu/ConnectionTest.js
@@ -1,15 +1,24 @@
 // 오픈비두 연결에 쓰일 테스트 컴포넌트
 import { Button } from "@chakra-ui/react";
-import { getSessionId } from "apis/api/TruthRoom";
+import {
+    closeOpenviduSession,
+    getOpenviduToken,
+    getSessionId,
+} from "apis/api/TruthRoom";
 import React, { useState } from "react";
 
 function ConnectionTest(props) {
-    const [sessionId, setSessionId] = useState(0);
+    const [sessionId, setSessionId] = useState("d");
+    const [openviduToken, setOpenviduToken] = useState("d");
 
     async function getSessionIdAndSave() {
         const gotSessionId = await getSessionId(5);
         setSessionId(gotSessionId);
-        console.log(sessionId);
+    }
+
+    async function getOpenviduTokenAndSave() {
+        const gotOpenviduToken = await getOpenviduToken(sessionId);
+        setOpenviduToken(gotOpenviduToken);
     }
 
     return (
@@ -17,7 +26,12 @@ function ConnectionTest(props) {
             <Button onClick={() => getSessionIdAndSave()}>
                 세션 id 받아오기
             </Button>
-            <Button>토큰 받아오기</Button>
+            <Button onClick={() => getOpenviduTokenAndSave()}>
+                토큰 받아오기
+            </Button>
+            <Button onClick={() => closeOpenviduSession(5)}>
+                세션 삭제하기
+            </Button>
         </div>
     );
 }
