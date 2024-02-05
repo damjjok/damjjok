aabdd42166c4c8d264f546291531d63deb997f97
손종민 <kain9101@naver.com>
2024-02-05 14:30:09 +0900

    Remove: PageIntroduction TitleText 컴포넌트로 통합

--- client/src/components/TitleText.js
+++ client/src/components/TitleText.js
@@ -1,12 +1,32 @@
 import { Box, Text } from "@chakra-ui/react";
 
 const TitleText = (props) => {
-    const { children, description, ...rest } = props;
+    const { children, description, img, ...rest } = props;
     return (
         <>
-            <Box borderLeft={"5px solid #FFD100"} paddingLeft={"1rem"} fontWeight={700}>
-                <Text {...rest}>{children}</Text>
-                <Text color={"#c0c0c0"}>{description}</Text>
+            <Box
+                width={"80vw"}
+                height={"100px"}
+                borderRadius={"xl"}
+                bgImage={`linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)), url(${img})`}
+                bgPosition="left"
+                paddingLeft={8}
+                paddingY={3}
+                marginY={2}
+                backdropContrast={"30%"}
+            >
+                <Box
+                    borderLeft={"5px solid #FFD100"}
+                    paddingLeft={"1rem"}
+                    fontWeight={700}
+                >
+                    <Text {...rest}>{children}</Text>
+                    <Text
+                    // color={"#c0c0c0"}
+                    >
+                        {description}
+                    </Text>
+                </Box>
             </Box>
         </>
     );
--- client/src/pages/group-space/group-space-main/last-challenge/LastChallenge.js
+++ client/src/pages/group-space/group-space-main/last-challenge/LastChallenge.js
@@ -1,15 +1,31 @@
 import { useLocation } from "react-router-dom";
 import StatusBar from "./status-bar/StatusBar";
 import HomeTab from "../group-tab/home-tab/HomeTab";
+// import PageIntroduction from "components/page-introduction/PageIntroduction";
+import TitleText from "components/TitleText";
+import bgSucceedChallenge from "assets/images/bgSucceedChallenge.png";
 
 function LastChallenge() {
     const location = useLocation();
     const challenge = location.state.challenge;
+    const tabName =
+        challenge.status === "success" ? "성공한 챌린지" : "실패한 챌린지";
+    const description =
+        challenge.status === "success"
+            ? "이전에 성공한 챌린지 정보를 볼 수 있어요"
+            : "이전에 실패한 챌린지 정보를 볼 수 있어요";
     return (
         <>
+            {/* <PageIntroduction tabName={tabName} /> */}
+            <TitleText
+                fontSize="2rem"
+                img={bgSucceedChallenge}
+                description={description}
+            >
+                {tabName}
+            </TitleText>
             <StatusBar challenge={challenge} />
-            <p>지난 챌린지</p>
-            <HomeTab />
+            <HomeTab challengeId={challenge.challengeId} />
             {/* props 전달해줄 것, HomeTab도 로직 수정해야 함 */}
             <p>{challenge.username}</p>
         </>
