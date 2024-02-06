a2c5e0e2e59f7ad5a06b7878a9d56ec07bb637a4
최명성 <audtjd1893@gmail.com>
2024-02-06 22:02:33 +0900

    Fix : CORS 문제 해결 해보자고~
    Change-Id: I51bb1c256377f9c8704b35c16ef3154a61e18f7b

--- server/src/main/java/com/ssafy/server/config/WebSocketConfig.java
+++ server/src/main/java/com/ssafy/server/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/truth-room-websocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/truth-room-websocket").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
