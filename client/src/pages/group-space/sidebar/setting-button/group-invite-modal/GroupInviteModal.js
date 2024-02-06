d0d9e2d2bbb37525b60da36956c8a552ef510ba5
손종민 <kain9101@naver.com>
2024-02-06 11:00:14 +0900

    Fix: GroupInviteModal CSS

--- client/src/pages/group-space/sidebar/setting-button/group-invite-modal/GroupInviteModal.js
+++ client/src/pages/group-space/sidebar/setting-button/group-invite-modal/GroupInviteModal.js
@@ -32,55 +32,55 @@ function GroupInviteModal() {
         <>
             <MenuItem onClick={onOpen}>멤버 초대하기</MenuItem>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>
-                        <p className=" font-extrabold">멤버 초대하기</p>
-                    </ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody py={10} px={5}>
-                        <Heading size="md" textAlign={"center"}>
-                            친구를 찾아보세요!
-                        </Heading>
-                        <Box px={6}>
-                            <SearchBar />
-                        </Box>
-                        <Heading size="md" textAlign={"center"}>
-                            주변 친구를 초대하세요!
-                        </Heading>
-                        <Box>
-                            <Flex
-                                my={4}
-                                justifyContent="center"
-                                alignItems="center"
-                            >
-                                <Text
-                                    as="u"
-                                    mr={4}
-                                    onClick={onCopy}
-                                    cursor={"pointer"}
+            {isOpen && (
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>
+                            <p className="font-extrabold">멤버 초대하기</p>
+                        </ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody py={10} px={5}>
+                            <Heading size="md" textAlign={"center"}>
+                                친구를 찾아보세요!
+                            </Heading>
+                            <Box px={6}>
+                                <SearchBar />
+                            </Box>
+                            <Heading size="md" textAlign={"center"}>
+                                주변 친구를 초대하세요!
+                            </Heading>
+                            <Box>
+                                <Flex
+                                    my={4}
+                                    justifyContent="center"
+                                    alignItems="center"
                                 >
-                                    {inviteCode}
-                                </Text>
-                                <IconButton>
-                                    <CopyIcon onClick={onCopy} />
-                                </IconButton>
-                            </Flex>
-                            {hasCopied ? (
-                                <SlideFade in={onCopy} offsetY={"20px"}>
-                                    <Alert status="success">
-                                        <AlertIcon />
-                                        초대 링크가 복사되었습니다!
-                                    </Alert>
-                                </SlideFade>
-                            ) : (
-                                ""
-                            )}
-                        </Box>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
+                                    <Text
+                                        as="u"
+                                        mr={4}
+                                        onClick={onCopy}
+                                        cursor={"pointer"}
+                                    >
+                                        {inviteCode}
+                                    </Text>
+                                    <IconButton>
+                                        <CopyIcon onClick={onCopy} />
+                                    </IconButton>
+                                </Flex>
+                                {hasCopied && (
+                                    <SlideFade in={onCopy} offsetY={"20px"}>
+                                        <Alert status="success">
+                                            <AlertIcon />
+                                            초대 링크가 복사되었습니다!
+                                        </Alert>
+                                    </SlideFade>
+                                )}
+                            </Box>
+                        </ModalBody>
+                    </ModalContent>
+                </Modal>
+            )}
         </>
     );
 }
