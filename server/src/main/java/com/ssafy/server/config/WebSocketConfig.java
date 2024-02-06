dbf2424955de169eed1d087ce3ca1195a38524f2
김다희 <kdahui@gmail.com>
2024-02-06 17:28:35 +0900

    Fix : 웹소켓 엔드포인트 cors 에러 수정

--- server/src/main/java/com/ssafy/server/config/WebSocketConfig.java
+++ server/src/main/java/com/ssafy/server/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/truth-room-websocket").withSockJS(); // 커넥션을 맺는 경로 설정. 만약 WebSocket을 사용할 수 없는 브라우저라면 다른 방식을 사용하도록 설정
+        registry.addEndpoint("/truth-room-websocket").setAllowedOrigins("*").withSockJS();
     }
 
 }
