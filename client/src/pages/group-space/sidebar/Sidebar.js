9d1fe2fe1a1076877aad606a96a13f2504567daa
jongmin Son <kain9101@naver.com>
2024-02-07 03:17:40 +0900

    Fix: Sidebar axios 연결

--- client/src/pages/group-space/sidebar/Sidebar.js
+++ client/src/pages/group-space/sidebar/Sidebar.js
@@ -2,8 +2,34 @@ import GroupList from "./group-list/GroupList";
 import ChallengeList from "./challenge-list/ChallengeList";
 import SettingButton from "./setting-button/SettingButton";
 import { Box, Flex } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getChallengeList } from "apis/api/Group";
 
 function Sidebar() {
+    const userId = 0;
+
+    const { groupId } = useParams();
+    // const setChallengeState = useSetRecoilState(challengeState);
+    const [currentChallengeList, setCurrentChallengeList] =
+        useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await getChallengeList(groupId);
+                const updatedChallengeList = response.list
+                setCurrentChallengeList(updatedChallengeList); // Recoil 상태에 데이터 적용
+
+            } catch (error) {
+                console.error("챌린지 정보 불러오기 실패", error);
+            }
+        };
+
+        fetchData(); // fetchData 함수 호출
+    }, [groupId]
+    );
+
     return (
         <Box borderRight={"1px solid rgba(214,214,214,0.25)"} height={"90vh"} minWidth={'250px'} width={"20vw"} maxWidth={'250px'}>
             <div className="flex flex-col h-300px px-4">
@@ -12,7 +38,7 @@ function Sidebar() {
                     <SettingButton />
                 </Flex>
                 <div className="my-8">
-                    <ChallengeList />
+                    <ChallengeList currentChallengeList={currentChallengeList} />
                 </div>
             </div>
         </Box>
