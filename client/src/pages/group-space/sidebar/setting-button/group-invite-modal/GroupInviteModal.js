c882dec8a6343efba6232f00dc3c8c79b322a745
손종민 <kain9101@naver.com>
2024-02-06 09:27:56 +0900

    Feat: 사이드바 회원 초대 모달

--- client/src/pages/group-space/sidebar/setting-button/group-invite-modal/GroupInviteModal.js
+++ client/src/pages/group-space/sidebar/setting-button/group-invite-modal/GroupInviteModal.js
@@ -1,30 +1,44 @@
-import { MenuItem, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react"
-
+import {
+    Heading,
+    MenuItem,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure,
+} from "@chakra-ui/react";
+import SearchBar from "pages/landing-page/create-group/create-group-modal/searchbar/SearchBar";
 
 // 초대 코드 생성 페이지 완료되면, 해당 코드 ModalBody 안에 붙여넣기
 function GroupInviteModal() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-    <>
-      <MenuItem onClick={onOpen}>멤버 초대하기</MenuItem>
+    return (
+        <>
+            <MenuItem onClick={onOpen}>멤버 초대하기</MenuItem>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>
-            <p className=" font-extrabold">
-              멤버 초대하기
-            </p>
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody py={10} px={20}>
-            <p>초대 코드 생성 페이지 그대로 붙여넣기</p>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
-  )
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        <p className=" font-extrabold">멤버 초대하기</p>
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody py={10} px={5}>
+                        <Heading size="md" textAlign={"center"}>
+                            친구를 찾아보세요!
+                        </Heading>
+                        <SearchBar />
+                        <Heading size="md" textAlign={"center"}>
+                            주변 친구를 초대하세요!
+                        </Heading>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+    );
 }
 
-export default GroupInviteModal
+export default GroupInviteModal;
