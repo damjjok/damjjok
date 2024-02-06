9e85a656ef7634c325dd89401fc9f74e9da23bca
손종민 <kain9101@naver.com>
2024-02-06 10:23:01 +0900

    Fix: RewardTab 스크롤 내부로 이동

--- client/src/pages/group-space/group-space-main/group-tab/reward-tab/RewardTab.js
+++ client/src/pages/group-space/group-space-main/group-tab/reward-tab/RewardTab.js
@@ -4,7 +4,7 @@ import RewardBox from "./reward-box/RewardBox";
 
 function RewardTab() {
     return (
-        <Box my={6}>
+        <Box my={6} overflowY={"auto"} height="50vh">
             <h1 className="text-xl font-bold">금연 저금통</h1>
             <PiggyBank />
             <p className="text-xl font-bold">나의 리워드</p>
