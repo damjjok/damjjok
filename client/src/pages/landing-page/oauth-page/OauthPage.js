c31bd6997d5d1b4f610a373bc06f37c13cd8c966
moonwlgh <moonwlgh@gmail.com>
2024-02-07 17:26:18 +0900

    Feat: accessToken decode 작업

--- client/src/pages/landing-page/oauth-page/OauthPage.js
+++ client/src/pages/landing-page/oauth-page/OauthPage.js
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 import { userState } from "contexts/Sns";
 import { useNavigate } from "react-router-dom";
 import { currentUser } from "contexts/User";
+import { jwtDecode } from "jwt-decode";
 
 const OauthPage = () => {
     const [user, setUser] = useRecoilState(userState);
@@ -18,6 +19,17 @@ const OauthPage = () => {
         const refreshToken = urlParams.get("refreshToken");
         const name = urlParams.get("name");
         const email = urlParams.get("email");
+
+        if (accessToken) {
+            try {
+                const decoded = jwtDecode(accessToken);
+
+                setCurrentUserInfo(decoded); // 토큰에서 추출한 사용자 정보를 저장
+            } catch (error) {
+                console.error("Error decoding token:", error);
+            }
+        }
+
         const userInfo = {
             name,
             email,
@@ -30,12 +42,16 @@ const OauthPage = () => {
             localStorage.setItem("accessToken", accessToken);
             localStorage.setItem("refreshToken", refreshToken);
         }
-    }, [navigate, setUser]); //
+    }, [navigate, setUser, setCurrentUserInfo]); //
 
     useEffect(() => {
         if (user.accessToken) navigate("/create-group");
         else if (user.email != "") navigate("/");
-    }, [user]);
+    }, [user, navigate]);
+    // currentUserInfo 상태의 변화를 감지하고 콘솔에 로그 출력
+    // useEffect(() => {
+    //     console.log("Updated currentUserInfo:", currentUserInfo);
+    // }, [currentUserInfo]);
 
     return <div>Oauth Redirect Page</div>;
 };
