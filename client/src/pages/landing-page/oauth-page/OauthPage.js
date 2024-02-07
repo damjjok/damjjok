cc4c01c7ca79579e6d8e832257397a25b975755c
문지호 <moonwlgh@gmail.com>
2024-02-07 22:37:00 +0900

    Feat: fcm 토큰 받기 및 axios로 보내기

--- client/src/pages/landing-page/oauth-page/OauthPage.js
+++ client/src/pages/landing-page/oauth-page/OauthPage.js
@@ -4,6 +4,7 @@ import { userState } from "contexts/Sns";
 import { useNavigate } from "react-router-dom";
 import { currentUser } from "contexts/User";
 import { jwtDecode } from "jwt-decode";
+import { requestForToken } from "pages/firebase/firebaseMessaging";
 
 const OauthPage = () => {
     const [user, setUser] = useRecoilState(userState);
@@ -20,16 +21,6 @@ const OauthPage = () => {
         const name = urlParams.get("name");
         const email = urlParams.get("email");
 
-        if (accessToken) {
-            try {
-                const decoded = jwtDecode(accessToken);
-
-                setCurrentUserInfo(decoded); // 토큰에서 추출한 사용자 정보를 저장
-            } catch (error) {
-                console.error("Error decoding token:", error);
-            }
-        }
-
         const userInfo = {
             name,
             email,
@@ -41,6 +32,18 @@ const OauthPage = () => {
         if (accessToken && refreshToken) {
             localStorage.setItem("accessToken", accessToken);
             localStorage.setItem("refreshToken", refreshToken);
+        }
+
+        if (accessToken) {
+            try {
+                const decoded = jwtDecode(accessToken);
+
+                setCurrentUserInfo(decoded); // 토큰에서 추출한 사용자 정보를 저장
+                // 로그인 성공 후 fcm 토큰 요청
+                requestForToken(accessToken);
+            } catch (error) {
+                console.error("Error decoding token:", error);
+            }
         }
     }, [navigate, setUser, setCurrentUserInfo]); //
 
@@ -54,6 +57,7 @@ const OauthPage = () => {
     // }, [currentUserInfo]);
 
     return <div>Oauth Redirect Page</div>;
+    //"https://i10e105.p.ssafy.io/api/v1/auth/fcmToken"
 };
 
 export default OauthPage;
