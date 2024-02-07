dce582cdb7288606b1c2e912040436154fc15092
박서현 <mentos0601@naver.com>
2024-02-07 15:15:07 +0900

    Feat : 토큰에 userId, email, userName 추가
    Change-Id: I08248592f687c335d546275f1628435af27754d3

--- server/src/main/java/com/ssafy/server/handler/OAuth2SuccessHandler.java
+++ server/src/main/java/com/ssafy/server/handler/OAuth2SuccessHandler.java
@@ -1,6 +1,7 @@
 package com.ssafy.server.handler;
 
 import com.ssafy.server.entity.TokenEntity;
+import com.ssafy.server.entity.UserEntity;
 import com.ssafy.server.provider.JwtProvider;
 import com.ssafy.server.repository.UserRepository;
 import jakarta.servlet.ServletException;
@@ -44,12 +45,13 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String encodedName = URLEncoder.encode(userName, StandardCharsets.UTF_8.toString());
 
 
-        String accessToken = jwtProvider.createToken(email, 5, ChronoUnit.SECONDS);
-        String refreshToken = jwtProvider.createToken(email, 5, ChronoUnit.SECONDS);
+        //boolean isExist = userRepository.existsByEmail(email);
+        UserEntity userEntity = userRepository.findByEmail(email);
 
-        boolean isExist = userRepository.existsByEmail(email);
+        if(userEntity != null){ // 가입되어있음 -> 바로 토큰 발행해서 줌
+            String accessToken = jwtProvider.createToken(userEntity.getUserId(),email,userName, 5, ChronoUnit.DAYS);
+            String refreshToken = jwtProvider.createToken(userEntity.getUserId(),email,userName, 5, ChronoUnit.DAYS);
 
-        if(isExist){ // 가입되어있음 -> 바로 토큰 발행해서 줌
             // redis 에 저장 ( refreshToken, email )
             ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
             valueOperations.set(refreshToken, email);
--- server/src/main/java/com/ssafy/server/provider/JwtProvider.java
+++ server/src/main/java/com/ssafy/server/provider/JwtProvider.java
@@ -12,6 +12,8 @@ import java.security.Key;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtProvider {
@@ -20,18 +22,25 @@ public class JwtProvider {
     private String secretKey;
 
     // 토큰 발급 공통 로직
-    public String createToken(String userId, long duration, ChronoUnit unit){
+    public String createToken(int userId, String email, String userName, long duration, ChronoUnit unit){
         Date expiredDate = Date.from(Instant.now().plus(duration, unit));
         Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", userId);
+        claims.put("email", email);
+        claims.put("userName", userName);
+
         return Jwts.builder()
                 .signWith(key, SignatureAlgorithm.HS256)
-                .setSubject(userId).setIssuedAt(new Date()).setExpiration(expiredDate)
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredDate)
                 .compact();
     }
 
-    public String validateToken(String token){
-        String subject = null;
+    public Jws<Claims> validateToken(String token){
+        Jws<Claims> subject = null;
         Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         try{
@@ -39,9 +48,7 @@ public class JwtProvider {
             subject = Jwts.parserBuilder()
                     .setSigningKey(key)
                     .build()
-                    .parseClaimsJws(token)
-                    .getBody()
-                    .getSubject();
+                    .parseClaimsJws(token);
 
         }catch (ExpiredJwtException e){
             throw new CustomJwtException("Token is Expired.",e);
