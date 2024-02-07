ed13cf1ce0421ddb396d3ceb566336710bde1015
김영후 <eagle7449@naver.com>
2024-02-07 23:36:25 +0900

    Fix: TruthRoom api 파일 테스트 코드 제거

--- client/src/apis/api/TruthRoom.js
+++ client/src/apis/api/TruthRoom.js
@@ -1,20 +1,5 @@
 import { axiosInstance } from "apis/instance/AxiosInstance";
 
-const postTest = async () => {
-    try {
-        const response = await axiosInstance.post("/test");
-        console.log(response.status);
-    } catch (error) {}
-};
-
-const getTest = async () => {
-    // 이런 식으로 쓰면 된다의 예시
-    try {
-        const response = await axiosInstance.get("/test"); // 호출해온 axiosInstance에 baseURL이 적혀있으므로 그 뒤부터 작성해주면 됨.
-        console.log(response.status);
-    } catch (error) {}
-};
-
 const getSessionId = async (propsSessionKey) => {
     const body = {
         sessionKey: propsSessionKey,
