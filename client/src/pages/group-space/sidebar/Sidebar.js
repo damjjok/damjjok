d4bf1e7ed828398cca8b8d72305ab0998435544e
jongmin Son <kain9101@naver.com>
2024-02-08 01:22:46 +0900

    Feat: Sidebar 모바일 UI

--- client/src/pages/group-space/sidebar/Sidebar.js
+++ client/src/pages/group-space/sidebar/Sidebar.js
@@ -1,26 +1,56 @@
 import GroupList from "./group-list/GroupList";
 import ChallengeList from "./challenge-list/ChallengeList";
 import SettingButton from "./setting-button/SettingButton";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Drawer, DrawerContent, DrawerOverlay, Flex, IconButton, useBreakpointValue, useDisclosure } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getChallengeList } from "apis/api/Group";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 function Sidebar() {
-
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const isMobile = useBreakpointValue({ base: true, md: false });
 
     return (
-        <Box borderRight={"1px solid rgba(214,214,214,0.25)"} height={"90vh"} minWidth={'250px'} width={"20vw"} maxWidth={'250px'}>
-            <div className="flex flex-col h-300px px-4">
-                <Flex justifyContent="space-between" alignItems="center" className="my-8">
-                    <GroupList />
-                    <SettingButton />
-                </Flex>
-                <div className="my-8">
-                    <ChallengeList />
+        <>
+              {/* 모바일 화면일 때 햄버거 버튼 노출 */}
+            {isMobile ? (
+                <IconButton
+                aria-label="Open menu"
+                icon={<HamburgerIcon />}
+                onClick={onOpen}
+                size="lg"
+                />
+        ) : (
+            <Box borderRight={"1px solid rgba(214,214,214,0.25)"} height={"90vh"} minWidth={'250px'} width={"20vw"} maxWidth={'250px'}>
+                <div className="flex flex-col h-300px px-4">
+                    <Flex justifyContent="space-between" alignItems="center" className="my-8">
+                        <GroupList />
+                        <SettingButton />
+                    </Flex>
+                    <div className="my-8">
+                        <ChallengeList />
+                    </div>
                 </div>
-            </div>
-        </Box>
+            </Box>)}
+
+                  {/* 모바일 화면에서 사이드바 역할을 하는 드로어 */}
+            <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+                <DrawerOverlay>
+                <DrawerContent>
+                    <div className="flex flex-col h-300px px-4">
+                    <Flex justifyContent="space-between" alignItems="center" className="my-8">
+                        <GroupList />
+                        <SettingButton />
+                    </Flex>
+                    <div className="my-8">
+                        <ChallengeList />
+                    </div>
+                    </div>
+                </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
     );
 }
 
