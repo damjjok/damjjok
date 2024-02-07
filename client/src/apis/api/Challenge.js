c87365a137e4a91aed704a27f1a314dbe6133fce
손종민 <kain9101@naver.com>
2024-02-07 16:31:12 +0900

    Fix: Challenge axios 수정

--- client/src/apis/api/Challenge.js
+++ client/src/apis/api/Challenge.js
@@ -1,19 +1,24 @@
 const { axiosInstance } = require("apis/instance/AxiosInstance");
 
-const createChallenge = async ({groupId, userId, initialMoney, savedMoney, savedPeriod}) => {
-  const body = {
-    groupId: groupId,
-    userId: userId,
-    initialMoney: initialMoney,
-    savedMoney: savedMoney,
-    savedPeriod: savedPeriod
-  }
-  try {
-      const response = await axiosInstance.post("/v1/challenge/create");
-      if (response.status === 200) return response.data
-      // console.log(response.status);
-  } catch (error) {}
+const createChallenge = async ({
+    groupId,
+    userId,
+    initialMoney,
+    savedMoney,
+    savedPeriod,
+}) => {
+    const body = {
+        groupId: groupId,
+        userId: userId,
+        initialMoney: initialMoney,
+        savedMoney: savedMoney,
+        savedPeriod: savedPeriod,
+    };
+    try {
+        const response = await axiosInstance.post("/v1/challenge/create", body);
+        if (response.status === 200) return response.data;
+        // console.log(response.status);
+    } catch (error) {}
 };
 
-
-export {createChallenge}
+export { createChallenge };
