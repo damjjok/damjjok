e52f1078073be57609232736866c77392660b7f6
손종민 <kain9101@naver.com>
2024-02-08 11:02:23 +0900

    Fix: CreateChallenge Duration에 recoilvalue 설정

--- client/src/pages/group-space/group-space-main/empty-challenge/create-challenge/CreateChallenge.js
+++ client/src/pages/group-space/group-space-main/empty-challenge/create-challenge/CreateChallenge.js
@@ -17,25 +17,31 @@ import ChallengeMoney from "./challenge-money/ChallengeMoney";
 import ChallengeCreateAlert from "./challenge-create-modal/ChallengeCreateAlert";
 import TitleText from "components/TitleText";
 import { useParams } from "react-router-dom";
+import { createChallengeState } from "contexts/Challenge";
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
 
 function CreateChallenge() {
     const { groupId } = useParams();
     const { isOpen, onClose } = useDisclosure();
-    // const [challenge, setChallenge] = useRecoilState(challengeState);
+    const [challenge, setChallenge] = useRecoilState(createChallengeState);
 
+    useEffect(() => {
+        console.log("createChallengeState has changed:", challenge);
+    }, [challenge]);
     // const endDate = useRecoilValue(challengeEndDate);
 
     return (
         <div>
             <TitleText
-                className="mt-4"
+                // className="mt-4"
                 fontSize="2rem"
                 img={bgCreateChallenge}
                 description="새로운 금연 챌린지를 생성해보세요!"
             >
                 챌린지 생성하기
             </TitleText>
-            <Flex flexFlow={"column"} alignItems={"center"}>
+            <Flex flexFlow={"column"} alignItems={"center"} mt={6}>
                 <VStack>
                     <ChallengeDuration />
                     <ChallengeMoney />
