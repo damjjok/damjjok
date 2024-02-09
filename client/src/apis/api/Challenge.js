f93bf2f2c1410d8100b7bd449b2ed8f923b01ee7
jongmin Son <kain9101@naver.com>
2024-02-09 19:22:56 +0900

    Fix: createChallenge API 바디 양식에 맞게 수정

--- client/src/apis/api/Challenge.js
+++ client/src/apis/api/Challenge.js
@@ -2,20 +2,20 @@ const { axiosInstance } = require("util/axios/AxiosInstance");
 
 const createChallenge = async ({
     groupId,
-    userId,
+    duration,
     initialMoney,
     savedMoney,
     savedPeriod,
 }) => {
     const body = {
         groupId: groupId,
-        userId: userId,
+        duration: duration,
         initialMoney: initialMoney,
         savedMoney: savedMoney,
         savedPeriod: savedPeriod,
     };
     try {
-        const response = await axiosInstance.post("/v1/challenge/create");
+        const response = await axiosInstance.post("/v1/challenge/create", body);
         if (response.status === 200) return response.data;
         // console.log(response.status);
     } catch (error) {}
