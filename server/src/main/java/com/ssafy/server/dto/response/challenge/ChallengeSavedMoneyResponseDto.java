b14ce07a1615974c939f6663a0fb87aa3879eb15
박서현 <mentos0601@naver.com>
2024-02-10 18:17:14 +0900

    Fix : 리워드 적립금 요청시 만료금액 추가
    Change-Id: Ie8367aad13002cfb99b5ec39fe462e1f14281c7e

--- server/src/main/java/com/ssafy/server/dto/response/challenge/ChallengeSavedMoneyResponseDto.java
+++ server/src/main/java/com/ssafy/server/dto/response/challenge/ChallengeSavedMoneyResponseDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.server.dto.response.challenge;
 
 import com.ssafy.server.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,19 @@ import org.springframework.http.ResponseEntity;
 @Getter
 public class ChallengeSavedMoneyResponseDto extends ResponseDto {
 
+    @Schema(description = "현재 누적 금액", example = "김다희")
     private int savedMoney;
+    @Schema(description = "챌린지 만료시 예상 금액", example = "김다희")
+    private int endMoney;
 
-    public ChallengeSavedMoneyResponseDto(int savedMoney){
+    public ChallengeSavedMoneyResponseDto(int savedMoney, int endMoney){
         super();
         this.savedMoney = savedMoney;
+        this.endMoney = endMoney;
     }
 
-    public static ResponseEntity<? super ChallengeSavedMoneyResponseDto> success (int savedMoney){
-        ChallengeSavedMoneyResponseDto responseBody = new ChallengeSavedMoneyResponseDto(savedMoney);
+    public static ResponseEntity<? super ChallengeSavedMoneyResponseDto> success (int savedMoney, int endMoney){
+        ChallengeSavedMoneyResponseDto responseBody = new ChallengeSavedMoneyResponseDto(savedMoney, endMoney);
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 }
