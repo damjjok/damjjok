6b63a56b47ccc5091b7163ec8fafc170fa9345d8
김영후 <eagle7449@naver.com>
2024-02-10 18:41:19 +0900

    Feat: 증거 판별 단계에서 다음으로 버튼 눌렀을 때 누른 멤버 표시 로직 구현

--- client/src/pages/truth-room/bottom-component/BottomComponent.js
+++ client/src/pages/truth-room/bottom-component/BottomComponent.js
@@ -1,5 +1,10 @@
-import React, { useContext } from "react";
-import { challengeIdState, stepState } from "contexts/TruthRoomSocket";
+import React, { useContext, useState } from "react";
+import {
+    challengeIdState,
+    joinMemberListState,
+    stepReadyCountState,
+    stepState,
+} from "contexts/TruthRoomSocket";
 import { useRecoilValue } from "recoil";
 import { Wrapper } from "./BottomComponent.style";
 import { Button } from "@chakra-ui/react";
@@ -9,17 +14,32 @@ function BottomComponent() {
     const { evidenceNextStage } = useContext(WebSocketContext);
     const step = useRecoilValue(stepState);
     const challengeId = useRecoilValue(challengeIdState);
+    const joinMemberList = useRecoilValue(joinMemberListState);
+    const stepReadyCount = useRecoilValue(stepReadyCountState);
+    const [isClickedReady, setIsClickedReady] = useState(false);
 
     function handleNextStepClick() {
         evidenceNextStage(challengeId);
+        setIsClickedReady(true);
     }
 
     if (step === 1)
         return (
             <Wrapper>
-                <Button colorScheme="yellow" onClick={handleNextStepClick}>
-                    다음으로
-                </Button>
+                {!isClickedReady && (
+                    <Button
+                        width={"80px"}
+                        colorScheme="yellow"
+                        onClick={handleNextStepClick}
+                    >
+                        다음으로
+                    </Button>
+                )}
+                {isClickedReady && (
+                    <Button width={"80px"}>
+                        {stepReadyCount}/{joinMemberList.length}
+                    </Button>
+                )}
             </Wrapper>
         );
     else return <Wrapper></Wrapper>;
