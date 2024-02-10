39de9c5a4770dd5c495b3538dd904daeb33b1212
김영후 <eagle7449@naver.com>
2024-02-10 19:00:04 +0900

    Fix: 벌금 결정 단계 컴포넌트 변수 소켓 관련으로 변경

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/fine-decision/FineDecisionFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/fine-decision/FineDecisionFrame.js
@@ -1,4 +1,7 @@
-import { fineDecisionStepState } from "contexts/TruthRoom";
+import {
+    enteringTruthRoomMemberInfoState,
+    fineStepState,
+} from "contexts/TruthRoomSocket";
 import React from "react";
 import { useRecoilValue } from "recoil";
 import DecideWaitingComponent from "./decide-waiting/DecideWaitingComponent";
@@ -6,19 +9,17 @@ import ResultComponent from "./result/ResultComponent";
 import FineInputFrame from "./decide/input/FineInputFrame";
 
 function FineDecisionFrame(props) {
-    const fineDecisionStep = useRecoilValue(fineDecisionStepState);
-    const testUser = {
-        name: "김영후",
-        role: "phD",
-    };
-
-    if (testUser.role === "damJJok" && fineDecisionStep !== 2) {
+    const fineStep = useRecoilValue(fineStepState);
+    const enteringTruthRoomMemberInfo = useRecoilValue(
+        enteringTruthRoomMemberInfoState
+    );
+    if (enteringTruthRoomMemberInfo.role === "damJJok" && fineStep !== 2) {
         return <DecideWaitingComponent />;
     } else {
         return (
             <div>
-                {fineDecisionStep === 0 && <FineInputFrame />}
-                {fineDecisionStep === 1 && <ResultComponent />}
+                {fineStep === 0 && <FineInputFrame />}
+                {fineStep === 1 && <ResultComponent />}
             </div>
         );
     }
