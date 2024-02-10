73f62b83c853afcdd68018cb223155204af90fd3
문지호 <moonwlgh@gmail.com>
2024-02-10 20:17:45 +0900

    Fix: 그룹이름 길떄 버튼밖으로 튀어나오는거 수정

--- client/src/pages/group-list-page/GroupListPage.js
+++ client/src/pages/group-list-page/GroupListPage.js
@@ -155,14 +155,23 @@ const GroupListPage = () => {
                                             size="xl" // 버튼 크기
                                             fontSize="30px" // 글자 크기
                                             colorScheme="blue" // 버튼 색상 스킴
-                                            px={10} // 좌우 패딩
-                                            py={7} // 상하 패딩
+                                            px={2} // 좌우 패딩
+                                            py={2} // 상하 패딩
                                             bg="#ffd100"
                                             width="150px"
                                             height="150px"
                                             onClick={() =>
                                                 handleGroupClick(group.groupId)
                                             }
+                                            style={{
+                                                whiteSpace: "normal", // 텍스트가 필요에 따라 줄바꿈되도록 설정
+                                                overflow: "hidden", // 내용이 넘칠 경우 숨김
+                                                textOverflow: "ellipsis", // 내용이 넘칠 경우 말줄임표로 표시 (여러 줄에 대해서는 작동하지 않음)
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                justifyContent: "center", // 텍스트를 버튼 중앙에 정렬
+                                                alignItems: "center", // 가로축 중앙 정렬
+                                            }}
                                         >
                                             {group.groupname}
                                         </Button>
