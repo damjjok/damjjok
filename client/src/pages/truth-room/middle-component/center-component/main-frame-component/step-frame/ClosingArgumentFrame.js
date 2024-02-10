1d8dd734ad23414c099978832626e9d0bc792c69
김영후 <eagle7449@naver.com>
2024-02-10 18:55:59 +0900

    Feat: 최후 변론에서 다음 단계 넘어가는 임시 함수 소켓 연결

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/ClosingArgumentFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/ClosingArgumentFrame.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { challengeIdState } from "contexts/TruthRoomSocket";
+import { WebSocketContext } from "contexts/WebSocketContext";
+import React, { useContext } from "react";
+import { useRecoilValue } from "recoil";
 
 function ClosingArgumentFrame(props) {
-    return <div>최후 변론</div>;
+    const { finishFinalArgument } = useContext(WebSocketContext);
+
+    const challengeId = useRecoilValue(challengeIdState);
+
+    function handleClickTest() {
+        // 최후 변론은 타이머가 다 되면 다음 단계로 넘어감. 이 로직 구현 전 임시로 클릭 시 다음 단계로 넘어가는 함수 매핑
+        finishFinalArgument(challengeId);
+    }
+
+    return <div onClick={handleClickTest}>최후 변론</div>;
 }
 
 export default ClosingArgumentFrame;
