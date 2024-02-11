ebb4f9eafc07d76950e15817118aff9a2603127a
jongmin Son <kain9101@naver.com>
2024-02-11 16:40:28 +0900

    Fix: App.js 원상복구

--- client/src/App.js
+++ client/src/App.js
@@ -121,10 +121,6 @@ function App() {
                         <Route path="/" element={<LandingPage />} />
                         <Route path="/group-list" element={<GroupListPage />} />
                         <Route
-                            path={`/group/:groupId`}
-                            element={<GroupSpaceMain />}
-                        />
-                        <Route
                             path={`/group/:groupId/*`}
                             element={<GroupSpacePage />}
                         >
