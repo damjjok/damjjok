14daa321d0e17083b67df3287f7353ecab4ffeb4
김영후 <eagle7449@naver.com>
2024-02-11 17:34:49 +0900

    Fix: rtc component 스크롤바와 중앙 맞추기 위해 좌측 패딩 적용

--- client/src/pages/truth-room/middle-component/right-component/rtc-component/RtcComponent.style.js
+++ client/src/pages/truth-room/middle-component/right-component/rtc-component/RtcComponent.style.js
@@ -9,6 +9,7 @@ export const Wrapper = styled.div`
     min-height: 65vh; // 최소 높이 설정
     max-height: 65vh; // 최대 높이 설정
     margin: 0 3vw;
+    padding-left: 8px; // 스크롤바와 맞추기 위해 왼쪽에 패딩 적용
     padding-bottom: 10px;
     border-radius: 20px;
     background-color: #515151;
