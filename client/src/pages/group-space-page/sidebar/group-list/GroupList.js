2af076f0c26321ef21ca876520f95c67098fd07e
jongmin Son <kain9101@naver.com>
2024-02-11 19:03:06 +0900

    Fix: Sidebar GroupList 페이지 링크 성공!

--- client/src/pages/group-space-page/sidebar/group-list/GroupList.js
+++ client/src/pages/group-space-page/sidebar/group-list/GroupList.js
@@ -12,14 +12,15 @@ function GroupList({ currentGroupInfo }) {
     const [groupListData, setGroupListData] = useState([]);
     const { groupId } = useParams();
     const groupIdVal = Number(groupId);
-    // 그룹 데이터를 가져오는 함수
-    useEffect(() => {
-        // 함수를 실행합니다.
-        fetchGroupData();
-    }, [groupListData, groupId]); // 빈 배열을 넘겨주어 컴포넌트 마운트 시에만 실행되도록 합니다.
 
     const navigate = useNavigate();
 
+    const [selectedGroup, setSelectedGroup] = useState({
+        value: currentGroupInfo.groupId,
+        label: currentGroupInfo.groupname,
+    });
+
+    // 그룹리스트 데이터 가져오기 함수
     const fetchGroupData = async () => {
         try {
             // getGroupList 함수를 호출하여 데이터를 가져옵니다.
@@ -40,6 +41,13 @@ function GroupList({ currentGroupInfo }) {
         }
     };
 
+    // 데이터 가져오기 함수 실행단계
+    useEffect(() => {
+        // 함수를 실행합니다.
+        fetchGroupData();
+    }, [groupId]); // URL 안의 그룹 ID가 바뀔 때 새로 데이터를 불러오는 걸로
+
+    // 동작 함수
     const handleGroupClick = (groupId) => {
         navigate(`/group/${groupId}`); // 해당 그룹 ID의 경로로 이동
     };
@@ -50,11 +58,7 @@ function GroupList({ currentGroupInfo }) {
         value: item.groupId,
         label: item.groupname,
     }));
-
-    const [selectedGroup, setSelectedGroup] = useState({
-        value: currentGroupInfo.groupId,
-        label: currentGroupInfo.groupname,
-    });
+    // console.log(options);
 
     const customStyles = {
         control: (base, state) => ({
@@ -62,6 +66,7 @@ function GroupList({ currentGroupInfo }) {
             backgroundColor: "#FFD100",
             borderColor: "white", // 프레임 색상을 흰색으로 설정
             boxShadow: state.isFocused ? 0 : 0, // 포커스 시 테두리 색상 제거
+            fontWeight: "600",
 
             "&:hover": {
                 borderColor: "white", // 마우스 호버 시 테두리 색상 유지
@@ -70,7 +75,11 @@ function GroupList({ currentGroupInfo }) {
         option: (styles, { isFocused, isSelected }) => {
             return {
                 ...styles,
-                backgroundColor: isSelected ? "rgba(255, 209, 0, 0.5)" : null,
+                backgroundColor: isSelected
+                    ? "rgba(255, 209, 0, 0.8)"
+                    : isFocused
+                    ? "rgba(255, 209, 0, 0.5)"
+                    : null,
                 color: "black",
                 cursor: "pointer",
                 "&:hover": {
@@ -89,19 +98,26 @@ function GroupList({ currentGroupInfo }) {
             ...base,
             backgroundColor: "black", // 구분선 색상을 흰색으로 설정
         }),
+        placeholder: (defaultStyles) => {
+            return {
+                ...defaultStyles,
+                color: "black", // 플레이스홀더 글씨 색상을 검정으로 설정
+                fontWeight: "600",
+            };
+        },
     };
 
     return (
         <Box flex={1} marginRight={"1rem"}>
             <Select
-                placeholder={selectedGroup.value}
-                defaultValue={selectedGroup.value}
+                placeholder={selectedGroup.label || currentGroupInfo.groupname}
+                defaultValue={selectedGroup.label || currentGroupInfo.groupname}
                 options={options}
-                value={selectedGroup.value}
+                value={selectedGroup.label}
                 styles={customStyles}
                 onChange={(selectedOption) => {
                     setSelectedGroup(selectedOption);
-                    handleGroupClick(selectedGroup.value);
+                    handleGroupClick(selectedOption.value);
                 }}
                 isSearchable={false}
                 style={{ flex: 1 }}
