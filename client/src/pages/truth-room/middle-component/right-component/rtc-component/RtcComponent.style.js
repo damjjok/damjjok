1afafbf1ae736fda00175370f6d545844e5d7481
김영후 <eagle7449@naver.com>
2024-02-11 17:23:04 +0900

    Fix: padding-bottom 추가

--- client/src/pages/truth-room/middle-component/right-component/rtc-component/RtcComponent.style.js
+++ client/src/pages/truth-room/middle-component/right-component/rtc-component/RtcComponent.style.js
@@ -9,6 +9,7 @@ export const Wrapper = styled.div`
     min-height: 65vh; // 최소 높이 설정
     max-height: 65vh; // 최대 높이 설정
     margin: 0 3vw;
+    padding-bottom: 10px;
     border-radius: 20px;
     background-color: #515151;
     overflow-y: scroll;
