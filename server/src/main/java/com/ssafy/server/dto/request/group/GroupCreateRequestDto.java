b2d558fa37d1f38a9fc11c7d31d4d96995306ca6
박서현 <mentos0601@naver.com>
2024-02-12 23:17:13 +0900

    Fix : list 도@NotNull 적용
    Change-Id: I2ba409fe20b0b51f005ad94cb2a335c98bc8b077

--- server/src/main/java/com/ssafy/server/dto/request/group/GroupCreateRequestDto.java
+++ server/src/main/java/com/ssafy/server/dto/request/group/GroupCreateRequestDto.java
@@ -3,6 +3,7 @@ package com.ssafy.server.dto.request.group;
 import com.ssafy.server.dto.group.UserInviteDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +21,7 @@ public class GroupCreateRequestDto {
     @Schema(description = "그룹명", example = "그룹명")
     private String name;
 
-    @NotBlank
+    @NotNull
     @Schema(description = "userId 만 담은 리스트", example = "list 형식")
     private List<UserInviteDto> list;
 
