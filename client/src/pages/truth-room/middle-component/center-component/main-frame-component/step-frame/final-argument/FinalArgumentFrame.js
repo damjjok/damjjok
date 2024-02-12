3d352dcd240e46b52587827947d02b5d808220bf
김영후 <eagle7449@naver.com>
2024-02-12 22:28:37 +0900

    Feat: 중앙 컴포넌트에 담쪽이 띄우는 기능 구현

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/final-argument/FinalArgumentFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/final-argument/FinalArgumentFrame.js
@@ -1,14 +1,21 @@
 import React from "react";
 import TimerComponent from "./TimerComponent";
 import { Wrapper } from "./FinalArgumentFrame.style";
+import { finalArgumentDamJJokState } from "contexts/TruthRoom";
+import { useRecoilValue } from "recoil";
+import UserVideoComponent from "pages/truth-room/openvidu/UserVideoComponent";
 
 function FinalArgumentFrame(props) {
+    const finalArgumentDamJJok = useRecoilValue(finalArgumentDamJJokState);
+
     return (
         <Wrapper>
             <div className="timer-container">
                 <TimerComponent />
             </div>
-            <div className="damJJok-frame">최후 변론</div>
+            <div className="damJJok-frame">
+                <UserVideoComponent streamManager={finalArgumentDamJJok} />
+            </div>
         </Wrapper>
     );
 }
