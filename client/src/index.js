cc43c3ecc83bf6b4eed18e1889db96f196daf8a3
손종민 <kain9101@naver.com>
2024-02-13 14:46:45 +0900

    Fix: index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
@@ -15,10 +16,12 @@ root.render(
                 <App />
             </WebSocketProvider>
         </RecoilRoot>
-    </BrowserRouter>
+    </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+serviceWorkerRegistration.register();
 reportWebVitals();
