e7d6fe669fb1cedfa3a875d45925fcf42492123a
최명성 <audtjd1893@gmail.com>
2024-02-14 00:31:51 +0900

    Fix : 리워드탭 가로 스크롤 삭제

--- client/src/pages/reward-tab-page/reword-box/RewardBox.js
+++ client/src/pages/reward-tab-page/reword-box/RewardBox.js
@@ -15,32 +15,22 @@ function RewardBox() {
     return (
         <div>
             {loginedUser.userId === challenge.userId ? (
-                <div
-                    className="relative flex justify-center my-8"
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                >
+                <div className="relative flex justify-center my-8" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                     <div className="" style={{ width: "300px" }}>
-                        <img
-                            src={postboxMain}
-                            alt="postboxMain"
-                            className=" saturate-0"
-                        />
+                        <img src={postboxMain} alt="postboxMain" className=" saturate-0" />
                     </div>
                     <div
                         className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] h-fit text-center py-4 bg-damlightgray rounded-lg transition-opacity duration-500 ${
                             isHovered ? "opacity-100" : "opacity-0"
                         }`}
                     >
-                        <p className=" font-bold">
-                            리워드는 챌린지가 끝난 후 확인할 수 있습니다!
-                        </p>
+                        <p className=" font-bold">리워드는 챌린지가 끝난 후 확인할 수 있습니다!</p>
                     </div>
                 </div>
             ) : (
                 <div className="flex flex-col items-center my-8">
                     <div className="flex justify-center my-4">
-                        <div style={{ width: "300px" }}>
+                        <div style={{ width: "10rem" }}>
                             <img src={postboxMain} alt="postboxMain" />
                         </div>
                     </div>
