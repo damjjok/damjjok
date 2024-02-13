04e1e41e67582d1e60df956128028652a84cdff4
김영후 <eagle7449@naver.com>
2024-02-13 14:27:42 +0900

    Fix: 타이머 위치 변경

--- client/src/pages/truth-room/bottom-component/BottomComponent.style.js
+++ client/src/pages/truth-room/bottom-component/BottomComponent.style.js
@@ -4,4 +4,11 @@ export const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+
+    .timer-container {
+        font-size: 50px;
+        font-weight: bold;
+        color: white;
+        margin-bottom: 20px;
+    }
 `;
