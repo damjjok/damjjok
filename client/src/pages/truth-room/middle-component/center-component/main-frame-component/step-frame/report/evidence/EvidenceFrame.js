3ab82019faea8c23512f0799da8cbe3d61cde1e6
최명성 <audtjd1893@gmail.com>
2024-02-14 15:54:42 +0900

    fix : saved Money 고침

--- client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/evidence/EvidenceFrame.js
+++ client/src/pages/truth-room/middle-component/center-component/main-frame-component/step-frame/report/evidence/EvidenceFrame.js
@@ -8,11 +8,15 @@ function EvidenceFrame() {
 
     return (
         <Wrapper>
-            <img
-                src={`https://i10e105.p.ssafy.io` + evidence.imagePath}
-                style={{ width: "100%", height: "100%" }}
-                alt="사진이 없어용"
-            />
+            {!evidence ? (
+                <Box>증거를 골라주세요.</Box>
+            ) : (
+                <img
+                    src={`https://i10e105.p.ssafy.io` + evidence.imagePath}
+                    style={{ width: "100%", height: "100%" }}
+                    alt="사진이 없어용"
+                />
+            )}
         </Wrapper>
     );
 }
