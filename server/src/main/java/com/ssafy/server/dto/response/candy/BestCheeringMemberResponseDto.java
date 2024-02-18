70de9bd92e21c66981a1bc9c4fafff86445206fb
박서현 <mentos0601@naver.com>
2024-02-18 19:06:40 +0900

    temp
    Change-Id: I5ca0bcc90475290081f91d5ed577435c75ea6565

--- server/src/main/java/com/ssafy/server/dto/response/candy/BestCheeringMemberResponseDto.java
+++ server/src/main/java/com/ssafy/server/dto/response/candy/BestCheeringMemberResponseDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.server.dto.response.candy;
 
+import com.ssafy.server.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 
 @Getter
 @Setter
-public class BestCheeringMemberResponseDto {
+public class BestCheeringMemberResponseDto extends ResponseDto {
     @Schema(description = "유저 이름", example = "김다희")
     private String userName;
     @Schema(description = "응원왕의 캔디 수", example = "16")
@@ -21,8 +22,8 @@ public class BestCheeringMemberResponseDto {
         this.candyCnt = candyCnt;
         this.cheerMsgCnt = cheerMsgCnt;
     }
-    public static ResponseEntity<CandyCountResponseDto> success(int count){
-        CandyCountResponseDto responseBody = new CandyCountResponseDto(count);
+    public static ResponseEntity<? super BestCheeringMemberResponseDto> success(String userName, int candyCnt, int cheerMsgCnt){
+        BestCheeringMemberResponseDto responseBody = new BestCheeringMemberResponseDto(userName, candyCnt, cheerMsgCnt);
         return ResponseEntity.ok(responseBody);
     }
 
